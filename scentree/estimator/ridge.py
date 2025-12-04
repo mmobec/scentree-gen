e55695e87239d1ef528f19724bffcd04b548454e
pachoning <cc.pachon@gmail.com>
2025-12-04 20:46:55 +0100

    Fix Ridge class format

--- scentree/estimator/ridge.py
+++ scentree/estimator/ridge.py
@@ -3,8 +3,15 @@ from sklearn.base import BaseEstimator
 from sklearn.linear_model import Ridge
 from typing import Optional, Type
 
+
 class RidgeEstimator(SklearnEstimator):
-    """Wrapper for the scikit-learn Ridge regression estimator."""
+    """Wrapper for the scikit-learn `Ridge` regression estimator.
+
+    Args:
+    estimator_class (Optional[Type[BaseEstimator]]):
+            Estimator class to wrap. Defaults to `sklearn.linear_model.Ridge`.
+    """
+
     def __init__(self, estimator_class: Optional[Type[BaseEstimator]] = None):
         if estimator_class is None:
             estimator_class = Ridge
