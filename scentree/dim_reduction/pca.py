a9300c803fd6a88511c613d7ba30f7c7ab53bab4
pachoning <cc.pachon@gmail.com>
2026-03-30 13:56:40 +0200

    Use mypy with PCA

--- scentree/dim_reduction/pca.py
+++ scentree/dim_reduction/pca.py
@@ -1,7 +1,8 @@
 import numpy as np
+from numpy.typing import NDArray
 from pydantic import BaseModel, PrivateAttr
 from sklearn.decomposition import PCA
-from typing import Optional, TypeVar
+from typing import cast, Optional, TypeVar
 
 R = TypeVar("R", bound="BasePCA")
 
@@ -16,48 +17,48 @@ class BasePCA(BaseModel):
 
     _reducer: Optional[PCA] = PrivateAttr(default=None)
 
-    def transform(self, X: np.ndarray) -> np.ndarray:
+    def transform(self, X: NDArray[np.float64]) -> NDArray[np.float64]:
         """
         Apply dimensionality reduction to a given matrix.
 
         Args:
-            X (np.ndarray): Data to be reduced.
+            X (NDArray[np.float64]): Data to be reduced.
 
         Raises:
             RuntimeError: If the method has not been fitted previously
                 (i.e., `self._reducer` is None).
 
         Returns:
-            np.ndarray: Data in the low-dimensional space.
+            NDArray[np.float64]: Data in the low-dimensional space.
         """
         if self._reducer is None:
             raise RuntimeError("Model has not been fitted yet.")
-        return self._reducer.transform(X)
+        return cast(NDArray[np.float64], self._reducer.transform(X))
 
-    def inverse_transform(self, X: np.ndarray) -> np.ndarray:
+    def inverse_transform(self, X: NDArray[np.float64]) -> NDArray[np.float64]:
         """
         Apply inverse transformation to obtain data in the high-dimensional space.
 
         Args:
-            X (np.ndarray): Data in the low-dimensional space.
+            X (NDArray[np.float64]): Data in the low-dimensional space.
 
         Raises:
             RuntimeError: If the method has not been fitted previously
                 (i.e., `self._reducer` is None).
 
         Returns:
-            np.ndarray: Data in the low-dimensional space.
+            NDArray[np.float64]: Data in the low-dimensional space.
         """
         if self._reducer is None:
             raise RuntimeError("Model has not been fitted yet.")
-        X_rec = self._reducer.inverse_transform(X)
+        X_rec = cast(NDArray[np.float64], self._reducer.inverse_transform(X))
         return X_rec
 
     def fit_auto_components(
         self,
-        X: np.ndarray,
+        X: NDArray[np.float64],
         threshold: float,
-    ) -> np.ndarray:
+    ) -> NDArray[np.float64]:
         """
         Fit a PCA reducer automatically selecting the number of components
         based on the explained variance threshold and transform the input data.
@@ -68,12 +69,12 @@ class BasePCA(BaseModel):
         fits a PCA reducer with this number of components and transforms the data.
 
         Args:
-            X (np.ndarray): Input feature matrix of shape (n_samples, n_features).
+            X (NDArray[np.float64]): Input feature matrix of shape (n_samples, n_features).
             threshold (float): Desired cumulative explained variance ratio (between 0 and 1)
                 used to select the number of principal components.
 
         Returns:
-            np.ndarray: Transformed input matrix with reduced dimensions based on
+            NDArray[np.float64]: Transformed input matrix with reduced dimensions based on
                 the automatically selected number of components.
         """
         pca_full = PCA()
@@ -81,5 +82,5 @@ class BasePCA(BaseModel):
         cumulative_variance = np.cumsum(pca_full.explained_variance_ratio_)
         n_components_th = np.argmax(cumulative_variance >= threshold) + 1
         self._reducer = PCA(n_components=n_components_th)
-        X_reduced = self._reducer.fit_transform(X)
+        X_reduced = cast(NDArray[np.float64], self._reducer.fit_transform(X))
         return X_reduced
