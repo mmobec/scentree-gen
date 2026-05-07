ba8ed708f58108d866492612db560626a7ca8032
pachoning <cc.pachon@gmail.com>
2026-05-07 16:34:04 +0200

    Allow range of values in datasets

--- scentree/io/loader.py
+++ scentree/io/loader.py
@@ -2,7 +2,11 @@ import numpy as np
 from numpy.typing import NDArray
 from pydantic import BaseModel, Field, model_validator
 from scentree.io import MapColumnsStages, MapColsNamesStages
-from typing import Dict, List, Optional, Self
+from typing import Dict, List, Optional, Self, Tuple
+
+
+# Typing representing a range (a, b) where a is less than b
+ValueRange = Optional[Tuple[float, float]]
 
 
 class DatasetSpec(BaseModel):
@@ -16,12 +20,14 @@ class DatasetSpec(BaseModel):
             It is used to break ties.
         all_positive (bool): Whether all columns are required to be positive.
             Default is False.
+        value_range (ValueRange): Optinal tuple indicating the range of values
+            for `values`.
     """
 
     values: NDArray[np.float64]
     stages: List[int]
     priority: List[int]
-    all_positive: bool = False
+    value_range: ValueRange = None
 
     model_config = {"arbitrary_types_allowed": True}
 
@@ -39,6 +45,8 @@ class DatasetSpec(BaseModel):
             - The length of `stages` must match the number of columns in `values`.
             - The length of `priority` must match the number of columns in `values`.
             - The prority must be unique within each stage.
+            - If `value_range` is not sorted, i.e., first value must be less than
+                second value.
 
         Returns:
             Self: The validated instance.
@@ -71,6 +79,9 @@ class DatasetSpec(BaseModel):
             i += 1
         if error_idx is not None:
             raise ValueError(f"Priority {error_idx[1]} is repeated in stage {error_idx[0]}")
+        if self.value_range is not None:
+            if self.value_range[0] > self.value_range[1]:
+                raise ValueError("`value_range[0]` must be less than or equal to value_range[1]`")
         return self
 
 
@@ -87,12 +98,16 @@ class DatasetLoader(BaseModel):
             It is automatically populated from `datasets_information`.
         dataset_names (List[List[str]]): The dataset names each column belongs to.
             It is automatically populated from ` datasets_information`.
+        value_ranges (Optional[List[List[ValueRange]]]):
+            Nested list containing the valid value range for each column of each dataset in
+            `stage_data`.
     """
 
     datasets_information: Dict[str, DatasetSpec]
     stage_ids: List[int] = Field(default_factory=list)
     stage_data: List[NDArray[np.float64]] = Field(default_factory=list)
     dataset_names: List[List[str]] = Field(default_factory=list)
+    value_ranges: Optional[List[List[ValueRange]]] = None
 
     model_config = {"arbitrary_types_allowed": True}
 
@@ -167,6 +182,43 @@ class DatasetLoader(BaseModel):
         return self
 
     @model_validator(mode="after")
+    def validate_value_ranges(self) -> Self:
+        """
+        Validate that all value ranges are properly defined.
+
+        This validator checks that, for every dataset in
+        `datasets_information`, the lower bound of `value_range`
+        is less than or equal to the upper bound whenever a
+        value range is provided.
+
+        Raises:
+            ValueError:
+                If a dataset contains an invalid value range where
+                `value_range[0] > value_range[1]`.
+
+        Returns:
+            Self:
+                The validated model instance.
+        """
+        i = 0
+        keys = list(self.datasets_information.keys())
+        total_keys = len(self.datasets_information.keys())
+        if self.value_ranges is not None:
+            while i < total_keys:
+                current_key = keys[i]
+                current_value_range = self.datasets_information[current_key].value_range
+                if (
+                    current_value_range is not None
+                    and current_value_range[0] > current_value_range[1]
+                ):
+                    raise ValueError(
+                        f"""`value_range[0]` must be less than or equal to value_range[1]`
+                        for dataset {current_key}"""
+                    )
+                i += 1
+        return self
+
+    @model_validator(mode="after")
     def fill_stage_ids(self) -> Self:
         """
         Populate the list of stage identifiers (`stage_ids`) from all datasets.
@@ -200,22 +252,29 @@ class DatasetLoader(BaseModel):
         This method iterates over all stage identifiers in `stage_ids` and collects
         the columns from each dataset in `datasets_information` that belong to the
         current stage. Columns are sorted according to their `priority` within the
-        stage. The resulting matrices for each stage are stored in `stage_data`,
-        and the corresponding ordered dataset names are stored in `dataset_names`.
+        stage. The resulting matrices for each stage are stored in `stage_data` and
+        the corresponding ordered dataset names and value ranges are stored in `dataset_names`
+        and `value_ranges` respectively.
 
         Returns:
             Self: The model instance with the `stage_data` attribute populated.
         """
         variables_sorted = []
         dataset_names: List[List[str]] = []
+        value_ranges: List[List[ValueRange]] = []
+        are_all_value_range_none: bool = True
         for stage in self.stage_ids:
             variables_stage = []
             priorities_stage = []
             dataset_names_stage = []
+            value_ranges_stage = []
             for key in self.datasets_information.keys():
                 info = self.datasets_information[key]
                 idx_variables_stage = [i for i, value in enumerate(info.stages) if value == stage]
                 dataset_name = len(idx_variables_stage) * [key]
+                dataset_value_ranges = len(idx_variables_stage) * [info.value_range]
+                if info.value_range is not None and are_all_value_range_none is True:
+                    are_all_value_range_none = False
                 # In case there is data for this stage, we sort the data accordingly
                 if len(idx_variables_stage) > 0:
                     priority_variables = [info.priority[i] for i in idx_variables_stage]
@@ -223,13 +282,20 @@ class DatasetLoader(BaseModel):
                     variables = info.values[:, idx_variables_stage]
                     variables_stage.append(variables)
                     dataset_names_stage.extend(dataset_name)
+                    value_ranges_stage.extend(dataset_value_ranges)
             variables_stage_matrix = np.concatenate(variables_stage, axis=1)
             idx_priorities_sorted = np.argsort(priorities_stage)
             dataset_names_stage_sorted = [dataset_names_stage[i] for i in idx_priorities_sorted]
             dataset_names.append(dataset_names_stage_sorted)
+            value_ranges_stage_sorted = [value_ranges_stage[i] for i in idx_priorities_sorted]
+            value_ranges.append(value_ranges_stage_sorted)
             variables_sorted.append(variables_stage_matrix[:, idx_priorities_sorted])
         self.stage_data = variables_sorted
         self.dataset_names = dataset_names
+        if are_all_value_range_none is False:
+            self.value_ranges = value_ranges
+        else:
+            self.value_ranges = None
         return self
 
     def to_stage_wise_loader(self) -> "StageWiseLoader":
@@ -239,8 +305,9 @@ class DatasetLoader(BaseModel):
         This method creates a `StageWiseLoader` instance where each element
         of `data` corresponds to the matrix of variables for a stage
         (from `stage_data`), `stage_names` contains the names of the
-        datasets associated with each stage and `mapping_columns_stages` maps
-        each column to its stage.
+        datasets associated with each stage, `mapping_columns_stages` maps
+        each column to its stage and `value_ranges` reports the range of values
+        for each column.
 
         Returns:
             StageWiseLoader: A new instance of `StageWiseLoader` containing
@@ -250,6 +317,7 @@ class DatasetLoader(BaseModel):
             data=self.stage_data,
             stage_ids=self.stage_ids,
             mapping_columns_stages=self.dataset_names,
+            value_ranges=self.value_ranges,
         )
         return stage_loader
 
@@ -268,6 +336,8 @@ class StageWiseLoader(BaseModel):
             of dataset names for each column in each stage. Each inner list
             must have the same length as the number of columns of the
             corresponding stage matrix.
+        value_ranges (Optional[List[List[ValueRange]]]):  Nested list containing
+            the valid value range for each column of each dataset in `data`.
         num_variables_per_stage (List[int]): Number of variables (columns)
             for each stage. It is automatically populated from `data`.
     """
@@ -275,6 +345,7 @@ class StageWiseLoader(BaseModel):
     data: List[NDArray[np.float64]]
     stage_ids: List[int]
     mapping_columns_stages: Optional[List[List[str]]] = None
+    value_ranges: Optional[List[List[ValueRange]]] = None
     num_variables_per_stage: List[int] = Field(default_factory=list)
 
     model_config = {"arbitrary_types_allowed": True}
@@ -313,6 +384,43 @@ class StageWiseLoader(BaseModel):
         if self.mapping_columns_stages is not None:
             if len(self.mapping_columns_stages) != len(self.data):
                 raise ValueError("`mapping_columns_stages` and `data` must be of the same length.")
+        return self
+
+    @model_validator(mode="after")
+    def validate_value_ranges(self) -> Self:
+        """
+        Validate the consistency between `value_ranges` and `data`.
+
+        This validator checks that:
+            - `value_ranges` and `data` contain the same number of stages.
+            - Each stage contains the same number of value ranges as
+              columns in the corresponding data matrix.
+
+        Raises:
+            ValueError:
+                - If `value_ranges` and `data` do not have the same length.
+                - If the number of value ranges for a stage does not match
+                  the number of columns in the corresponding dataset.
+
+        Returns:
+            Self:
+                The validated model instance.
+        """
+        if self.value_ranges is not None:
+            if len(self.value_ranges) != len(self.data):
+                raise ValueError("`value_ranges` and `data` must be of the same length.")
+            i = 0
+            total_stages = len(self.data)
+            while i < total_stages:
+                num_columns = self.data[i].shape[1]
+                num_ranges = len(self.value_ranges[i])
+                if num_columns != num_ranges:
+                    raise ValueError(
+                        f"""Dataset in position {i} contains {num_columns} columns.
+                        List of `value_ranges` in position {i} contains
+                        {num_ranges} values. Both numbers must be equal."""
+                    )
+                i += 1
         return self
 
     @model_validator(mode="after")
@@ -378,6 +486,22 @@ class StageWiseLoader(BaseModel):
                 concatenated column-wise.
         """
         return np.concatenate(self.data, axis=1)
+
+    def create_full_range_values(self) -> Optional[List[ValueRange]]:
+        """Flatten the nested `value_ranges` structure into a single list.
+        This method concatenates the value ranges of all stages into a
+        one-dimensional list preserving the original column order across
+        stages.
+
+        Returns:
+            Optional[List[ValueRange]]: A flattened list containing the value
+            ranges for all columns across all stages. Returns None
+            if `value_ranges` is not defined.
+        """
+        results = None
+        if self.value_ranges is not None:
+            results = [x for vr in self.value_ranges for x in vr]
+        return results
 
     def build_stages_columns_mapping(self) -> MapColsNamesStages:
         """
